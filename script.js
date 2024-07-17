c68f47b72bd437141936c732d940ed0540031595
jamiep <yeojinp2458@gmail.com>
2024-07-17 12:38:29 -0500

    event

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-// script.js
-
-let currentSlide = 0;
-let slides = [];
-let slideTexts = [];
+var currentSlideIndex = 0;
+var slides = [];
+var slideTexts = [];
 
 function expandImage(element) {
   var popup = document.getElementById("imagePopup");
   var popupImage = document.getElementById("popupImage");
   var popupText = document.getElementById("popupText");
 
-  // Get the text from data-text attribute
-  var text = element.getAttribute("data-text");
+  // Get the image source and data-text from the clicked element's child img
+  var img = element.querySelector("img");
+  var imageUrl = img.src;
+  var text = img.getAttribute("data-text");
 
-  // Replace \n with <br> to create line breaks
+  // Replace \n with <br> to create line breaks in the popup text
   var formattedText = text.replace(/\n/g, "<br>");
 
+  // Display the popup with the corresponding image and text
+  popup.style.display = "flex";
+  popupImage.src = imageUrl;
+  popupText.innerHTML = formattedText;
+
+  // Update the slides array and slideTexts array
   slides = Array.from(document.querySelectorAll(".event-item img")).map(img => img.src);
   slideTexts = Array.from(document.querySelectorAll(".event-item img")).map(img => img.getAttribute('data-text'));
 
-  currentSlide = slides.indexOf(element.querySelector("img").src);
-  popup.style.display = "flex";
-  popupImage.src = slides[currentSlide];
-  popupText.innerHTML = formattedText;
+  // Set the current slide index
+  currentSlideIndex = slides.indexOf(imageUrl);
+}
 
+function changeSlide(direction) {
+  currentSlideIndex += direction;
+
+  // Wrap around if exceeding the slide boundaries
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  } else if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+
+  // Update the popup image and text
+  popupImage.src = slides[currentSlideIndex];
+  popupText.innerHTML = slideTexts[currentSlideIndex];
 }
 
 function closeImage() {
   var popup = document.getElementById("imagePopup");
   popup.style.display = "none";
-}
-
-function changeSlide(direction) {
-  currentSlide += direction;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  } else if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-  var popupImage = document.getElementById("popupImage");
-  var popupText = document.getElementById("popupText");
-
-  popupImage.src = slides[currentSlide];
-  popupText.textContent = slideTexts[currentSlide];
-
 }
